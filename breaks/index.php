62d50d3ba30354abefd63acbf41421620a61ab03
Anant kumar <anant.kumar1989@gmail.com>
2017-06-18 13:54:44 +0530

    First commit. All post APIs

---
+++ breaks/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+	header("Content-Type:application/json");
+	
+	include("../database.php");
+		
+	$data = json_decode(file_get_contents('php://input'), true);
+		
+	if(!empty($data)){
+		
+		$e = $data['employee_id'];
+		$ev = $data['event'];
+		$d = $data['date'];
+		$t = $data['time'];
+		$query = "insert into entery (id,employee_id,event,date,time,type) 
+				VALUES ('','$e','$ev','$d','$t','B')";
+				
+		$result = mysql_query($query);
+		echo $result;exit;;
+	}
+	
+?>
---
+++ database.php
@@ -0,0 +1,5 @@
+ <?php 
+	// Connects to my Database 
+	mysql_connect("localhost", "root") or die(mysql_error()); 
+	mysql_select_db("attendance") or die(mysql_error()); 
+ ?> 
